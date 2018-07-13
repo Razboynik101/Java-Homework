f16a666dfeb15fd6464ff4d847996f402821f496
Razboynik101 <Razboynik101@yandex.ru>
2018-07-13 23:50:45 +0300

    Created project

---
+++ src/Lesson_2.java
@@ -0,0 +1,23 @@
+public class Lesson_2 {
+
+        public static void Counter(int a[][]){
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+        public static void main(String[] args) {
+            String[][][][] arr = {{"2","6","1","4"} {3,4}};
+            Counter(arr);
+        }
+
+
+}
