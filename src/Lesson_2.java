a2859005ab95c592e92eb19fe9ee788738cf5b38
Razboynik101 <Razboynik101@yandex.ru>
2018-07-16 11:50:49 +0300

    сделал 2 урок

--- src/Lesson_2.java
+++ src/Lesson_2.java
@@ -17,7 +17,18 @@ try {
 
 
         public static void main(String[] args) {
-            String[][][][] arr = new String[][][][]{};
+            int SIZE = 4;
+            String[][] arr = new String[SIZE][SIZE];
+            for (int i = 0; i <SIZE ; i++) {
+                for (int j = 0; j <SIZE ; j++) {
+                    Random rnd = new Random;
+
+                    arr[i][j]= rnd.nextInt(10);
+
+                }
+
+            }
+            String[][] arr = new String[][][][]{};
             Counter(arr);
         }
 
